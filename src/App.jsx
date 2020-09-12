8be8ad2dbd2b92778afee996edf3d2354dafb2d5
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2020-09-12 17:53:41 -0400

    creating routes for list and favorites

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React, {useState, useEffect, useContext} from 'react';
 import './App.css';
+import { Switch, Route, Redirect } from "react-router";
+
 import {FavoriteContext} from './context/FavoriteProvider'
 import { fetchAllColors } from './service'
+import Home from "./components/Home"
+import PostList from "./components/PostList"
 
 function App() {
   const {updateFavorites} = useContext(FavoriteContext);
@@ -10,8 +14,8 @@ function App() {
 
   useEffect(() => {
     fetch("https://reqres.in/api/unknown")
-        .then(res => res.json())
-        .then(response => setColors(response.data))
+    .then(res => res.json())
+    .then(response => setColors(response.data))
 
   }, [])
 
@@ -19,22 +23,38 @@ function App() {
   return (
 
     <div className = 'App'>
+      <Switch>
+        <Route exact path="/" render={(routerProps) => {
+            return <Home history={routerProps.history} />;
+          }} />
+
+        <Route exact path="/feed" render={(routerProps) => {
+            return <PostList history={routerProps.history} />
+        }} />
+
+        <Route exact path="/favorites" render={(routerProps) => {
+            return <PostList history={routerProps.history} />;
+        }} />
+
+        <Redirect to="/feed" />
+      </Switch>
+
+
 
       <div id="colorList">
 
-
-          {colors.map((color) => {
-            return (
-              <div>
-                <h1>DEPLOY WORKED</h1>
+        {colors.map((color) => {
+          return (
+            <div>
+              <h1>DEPLOY WORKED</h1>
               <div className="colorOption" id={color.id} key={color.id} >
                 <span className="colorLabel"> {color.name}</span>
                 <span className="colorBlob" style={{backgroundColor:`${color.color}`}} onClick={() => updateFavorites(color.color.slice(1), color)}> </span>
               </div>
             </div>
-            )
-          })}
-        </div>
+          )
+        })}
+      </div>
 
 
 
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostItem = () => {
+  return (
+    <div>
+      Post
+    </div>
+  )
+}
+
+export default PostItem
